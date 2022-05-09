3359e743ed21e8cb94ce3c53dad09c101a162061
amir-ys <amir.you74@gmail.com>
2022-05-09 23:23:13 +0430

    move common method in repositoryInterface to BaseInterface Class

--- modules/AttributeGroup/Contracts/AttributeGroupRepositoryInterface.php
+++ modules/AttributeGroup/Contracts/AttributeGroupRepositoryInterface.php
@@ -2,11 +2,9 @@
 
 namespace Modules\AttributeGroup\Contracts;
 
-interface AttributeGroupRepositoryInterface
+use Modules\Core\Contracts\BaseRepositoryInterface;
+
+interface AttributeGroupRepositoryInterface extends BaseRepositoryInterface
 {
     public function getAllPaginate();
-    public function findById(int $id);
-    public function store(array $data);
-    public function update(int $id,array $data);
-    public function destroy(int $id);
 }
