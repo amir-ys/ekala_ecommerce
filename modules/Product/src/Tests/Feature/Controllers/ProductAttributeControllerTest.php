c7316655399d21e8b2d70795165004f0362ab5b9
amir-ys <amir.you74@gmail.com>
2022-06-03 16:44:42 +0430

    complete product attribute controller tests

--- modules/Product/src/Tests/Feature/Controllers/ProductAttributeControllerTest.php
+++ modules/Product/src/Tests/Feature/Controllers/ProductAttributeControllerTest.php
@@ -2,6 +2,7 @@
 namespace Modules\Product\Tests\Feature\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Attribute\Models\Attribute;
 use Modules\Product\Models\Product;
 use Tests\TestCase;
 
@@ -19,5 +20,17 @@ class ProductAttributeControllerTest extends TestCase
             ->assertViewHas([
                 'product' => $product
             ]);
+    }
+
+    public function test_product_attribute_can_save()
+    {
+        $product = Product::factory()->create();
+        $attribute =  Attribute::factory()->create();
+        $data = [ 'attributes' => [ $attribute->id => '::attribute-value::' ] ];
+        $this->post(route('panel.products.attributes.save' , $product->id) , $data);
+
+        $this->assertDatabaseCount('attribute_product' , 1);
+        $this->assertDatabaseHas('attribute_product' , [ 'product_id' =>$product->id ,
+            'attribute_id' => $attribute->id , 'value' => $data['attributes'][$attribute->id]]);
     }
 }
