9c46afbf298f9aae65c1c5d939dd80f9313c948a
amir-ys <amir.you74@gmail.com>
2022-06-04 17:20:17 +0430

    add laravel/breeze packages for authentication

--- modules/User/src/Providers/UserServiceProvider.php
+++ modules/User/src/Providers/UserServiceProvider.php
@@ -1,17 +1,36 @@
 <?php
 namespace Modules\User\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider
 {
+    private  string $namespace = "Modules\User\Http\Controllers";
     public function register()
     {
-        
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views' , 'User');
+        $this->loadUserRoute();
+        $this->loadAuthRoute();
+
     }
 
     public function boot()
     {
-        
+
+    }
+
+    private function loadUserRoute()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(__DIR__ . '/../Routes/auth_routes.php');
+    }
+
+    private function loadAuthRoute()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(__DIR__ . '/../Routes/auth_routes.php');
     }
 }
